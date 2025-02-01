60a15b24b0a36903dca9fd7500821acd04524e7e
JD1985FR <jimenez.girard.david@gmail.com>
2025-02-02 00:32:04 +0100

    test_rust

---
+++ target/debug/deps/untitled-963b8fc1ecfc3e2f.d
@@ -0,0 +1,5 @@
+/Users/davidjimenez-girard/RustroverProjects/untitled/target/debug/deps/untitled-963b8fc1ecfc3e2f: src/main.rs
+
+/Users/davidjimenez-girard/RustroverProjects/untitled/target/debug/deps/untitled-963b8fc1ecfc3e2f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/untitled.d
@@ -0,0 +1,1 @@
+/Users/davidjimenez-girard/RustroverProjects/untitled/target/debug/untitled: /Users/davidjimenez-girard/RustroverProjects/untitled/src/main.rs
